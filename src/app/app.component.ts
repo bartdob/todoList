c018b4789d82192aa2f3411236f01e911b043a22
Bart <44099512+bartdob@users.noreply.github.com>
2023-12-19 14:09:48 +0100

    add bootsrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { TaskListComponent } from "./task-list.component";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ParseSourceFile } from '@angular/compiler';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     template: `
+    <div class="container fluid text-center">
     <h1>ToDo list</h1>
-    <div class="text-center">
-      <input #taskNameInput class="border-b border-b-black-400 outline-none"/>
-      <button (click)="addTask(taskNameInput.value)" type="button" class="border border-orange-500 ml-4">Add</button>
-      <app-task-list [tasks]="tasks"/>
+      <div class="text-center">
+        <input #taskNameInput class="form-control w-50"/>
+        <button (click)="addTask(taskNameInput.value)" type="button" class="btn btn-warning">Add</button>
+        <app-task-list [tasks]="tasks"/>
+      </div>
     </div>
   `,
     imports: [NgFor, TaskListComponent, TooltipModule]
 })
 export class AppComponent {
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
   tasks = [
     {
       name: "job 1",
